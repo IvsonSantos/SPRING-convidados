74ab9c2f9b47f8e6a707c44c870681dfc6425987
Ivson Santos <santosivson@hotmail.com>
2018-03-24 16:32:45 -0300

    Banco de dados in memory

--- src/main/java/com/ivson/spring/controller/ConvidadoController.java
+++ src/main/java/com/ivson/spring/controller/ConvidadoController.java
@@ -3,25 +3,32 @@ package com.ivson.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ivson.spring.model.Convidado;
 import com.ivson.spring.repository.ConvidadoRepository;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadoController {
 
 	@Autowired
 	private ConvidadoRepository convidadoRepository;
 	
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		modelAndView.addObject("convidados", convidadoRepository.findAll());
+		modelAndView.addObject(new Convidado());
 		return modelAndView;
 	}
 	
-	@GetMapping("/oi")
-	public String getOi() {
-		return "welcome";
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		this.convidadoRepository.save(convidado);
+		return "redirect:/convidados";
 	}
+	
 }
